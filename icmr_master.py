0a748fdfcc10c236b0162033067ba79cffdbc0e9
GMCHNKL <73467282+GMCHNKL@users.noreply.github.com>
2021-01-06 19:19:06 +0530

    Update icmr_master.py

--- icmr_master.py
+++ icmr_master.py
@@ -2,11 +2,16 @@ import icmr
 import Distripute
 import sys
 import basedir as bs
+import mergefiles as mf
 
 # python icmr_master.py start end (which instance)
 
 datadict = []
+try: icmrid = str(sys.argv[3])
+except: icmrid = '1'
 resultData = bs.BaseDir+'DataRemains/ResultData.txt'
+for i in icmrid:
+  mf.merge(i)
 tag = icmr.file_exist(resultData)
 print("Tag :",tag)
 datadict = icmr.getDatadict(resultData)
